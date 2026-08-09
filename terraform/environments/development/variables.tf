b96920e70dd732d83f131161c1f9cd196e72b9bf
Piyush Vardhan <piyushvardhan@yahoo.com>
2026-08-10 00:12:58 +0200

    fix(terraform): remove unused location variable from dev root

--- terraform/environments/development/variables.tf
+++ terraform/environments/development/variables.tf
@@ -6,10 +6,6 @@ variable "resource_group_name" {
   description = "The name of the resource group in which to create the Azure Bastion resource."
   type        = string
 }
-variable "location" {
-  description = "The Azure region in which to create the Azure Bastion resource."
-  type        = string
-}
 variable "vnet_name" {
   description = "The name of the Virtual Network."
   type        = string
